1830e85801fc566c26ef7c556acd1ac1b4cd5f07
Aleksandra Zdrojowa <a.zdrojowa@gmail.com>
2020-09-16 00:57:43 +0200

    add swagger doc in order to implement and check cache

---
+++ src/main/java/webservice/moneyback/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package webservice.moneyback;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+
+    @Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(apiInfo())
+				.select()
+				.apis( RequestHandlerSelectors.basePackage("webservice.moneyback"))
+	            .paths(PathSelectors.any())
+	            .build();
+	}
+    
+    private ApiInfo apiInfo() {
+    return new ApiInfoBuilder()
+            .title("Moneyback API with Swagger")
+            .contact(new Contact("Aleksandra Zdrojowa",
+            					"website here",
+                                "a.zdrojowa@gmail.com"))
+            .build();
+}
+    
+
+
+ 
+}

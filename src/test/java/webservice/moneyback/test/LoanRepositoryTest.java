aeaefb9057736856b1246a2e2595146454e34b65
Aleksandra Zdrojowa <a.zdrojowa@gmail.com>
2020-09-22 23:23:24 +0200

    upgrading integration test

--- src/test/java/webservice/moneyback/test/LoanRepositoryTest.java
+++ src/test/java/webservice/moneyback/test/LoanRepositoryTest.java
@@ -1,15 +1,19 @@
 package webservice.moneyback.test;
 
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.Date;
 
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import junit.framework.Assert;
@@ -37,8 +41,7 @@ public class LoanRepositoryTest {
 		String expected = l1.getForwho();
 		String result = loanRepo.findById(1).getForwho();
 		
-		
-		Assert.assertEquals(expected, result);
+		assertEquals(expected,result);
 	}
 
 	
